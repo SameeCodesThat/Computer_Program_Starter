148bd354a0c4cd4fc96f716c83e69b325b6ddb33
sameeshahid <sameeshahidss@gmail.com>
2025-12-12 16:09:04 +0000

    modified config for session cookies, session lifetime, password pepper, bio key, development debugging

--- config.py
+++ config.py
@@ -1,5 +1,31 @@
+from datetime import timedelta
+from flask import Flask
+import os
+
+app = Flask(__name__)
+#-------------------- secure session management (CWE-614) -------------------
+
 class Config:
     DEBUG = True
     SECRET_KEY = 'supersecretkey'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    app.config['SESSION_COOKIE_HTTPONLY'] = True
+    app.config['SESSION_COOKIE_SECURE'] = True
+    app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
+    app.config['SESSION_REFRESH_EACH_REQUEST'] = True
+    app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=30)
+    PASSWORD_PEPPER = os.getenv("PASSWORD_PEPPER")
+    BIO_KEY = os.getenv("BIO_ENCRYPTION_KEY")
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    ENV = "development"
+
+class ProductionConfig(Config):
+    DEBUG = False
+    ENV = "production"
+    app.config['SESSION_COOKIE_HTTPONLY'] = True
+    app.config['SESSION_COOKIE_SECURE'] = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
